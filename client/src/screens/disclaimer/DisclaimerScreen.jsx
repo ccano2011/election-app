0614b55feb23d76e4013acd51941858fbcdd11d6
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-10-30 12:27:23 -0400

    disclaimer screen

---
+++ client/src/screens/disclaimer/DisclaimerScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DisclaimerScreen.css';
+import Nav from '../../components/shared/Nav/Nav'
+
+
+function DisclaimerScreen() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default DisclaimerScreen;
