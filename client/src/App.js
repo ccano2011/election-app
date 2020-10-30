3bc24c1bbd1dca5839adb9e77c24b5a6ec1f9cee
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-10-30 16:37:22 -0400

    styled and completed the disclaimer screen and styling; beginning work on admin

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import Home from '../src/screens/home/Home'
 import Nav from './components/shared/Nav/Nav'
 import RequestBallotScreen from "./screens/requestballot/RequestBallotScreen"
-import { Route, Switch} from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
+import DisclaimerScreen from "./screens/disclaimer/DisclaimerScreen";
 
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path="/request-ballot" component = {RequestBallotScreen}/>
+        <Route exact path='/' component={Home} />
+        <Route path='/disclaimer' component={DisclaimerScreen} />
+        <Route path="/request-ballot" component={RequestBallotScreen} />
       </Switch>
     </div>
   );
