cc69d5d66a9c797ef6b21490cbbcd910a9c5300a
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-11-06 15:36:47 -0500

    initial create-account-screen functional; posting to backend

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import RequestConfirmed from "./screens/requestconfirmed/RequestConfirmedScreen"
 import AdminScreen from "./screens/admin/AdminScreen"
 import EditBallotScreen from "./screens/requestballot/EditBallotScreen"
 import BallotStatusScreen from "./screens/ballotstatus/BallotStatusScreen";
+import CreateAccountScreen from "./screens/createaccount/createAccountScreen";
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
         <Route path="/admin" component={AdminScreen} />
         <Route path="/edit-ballot/:id" component={EditBallotScreen} />
         <Route path="/request-confirmed" component={RequestConfirmed} />
+        <Route path="/create-account" component={CreateAccountScreen} />
       </Switch>
     </div>
   );
