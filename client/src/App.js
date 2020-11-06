83f3ab0c68b50409b76b18d44e8b3407298cbcfd
Brian <briand.nester@gmail.com>
2020-11-06 10:20:54 -0500

    commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './App.css';
+import Home from '../src/screens/home/Home'
+import Nav from './components/shared/Nav/Nav'
+import RequestBallotScreen from "./screens/requestballot/RequestBallotScreen"
+import { Route, Switch } from "react-router-dom"
+import DisclaimerScreen from "./screens/disclaimer/DisclaimerScreen";
+import RequestConfirmed from "./screens/requestconfirmed/RequestConfirmedScreen"
+import AdminScreen from "./screens/admin/AdminScreen"
+import EditBallotScreen from "./screens/requestballot/EditBallotScreen"
+import BallotStatusScreen from "./screens/ballotstatus/BallotStatusScreen";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Nav />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/status' component={BallotStatusScreen} />
+        <Route path='/disclaimer' component={DisclaimerScreen} />
+        <Route path="/request-ballot" component={RequestBallotScreen} />
+        <Route path="/admin" component={AdminScreen} />
+        <Route path="/edit-ballot/:id" component={EditBallotScreen} />
+        <Route path="/request-confirmed" component={RequestConfirmed} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
