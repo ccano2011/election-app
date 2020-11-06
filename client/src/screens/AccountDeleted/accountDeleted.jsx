946935fe2872a27e1986dd0636a2b98136888a13
Danielle <danibrodwater@outlook.com>
2020-11-06 15:58:51 -0300

    Account delete and deleted screens

---
+++ client/src/screens/AccountDeleted/accountDeleted.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './accountDeleted.css'
+import Nav from "../../components/shared/Nav/Nav"
+import ArrowImg from '../../Assets/left-arrow.svg';
+import { Link } from "react-router-dom"
+
+
+function accountDeleted(props) {
+  return (
+    <div>
+     <Nav/>
+     <div className='subHeader'>
+            <Link to='/'>
+              <img className='arrow' src={ArrowImg} alt='arrow' height='25' weight='25' />
+            </Link>
+            <p className="headline">Account Deleted</p>
+      </div>
+      <div className='deleted-p'>
+        <p>Sorry to see you go!</p>
+      </div>
+      <div className='button'>
+        <Link to='/'><button className='backHomeBtn'>Home</button></Link>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default accountDeleted;
