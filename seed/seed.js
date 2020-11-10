0e64c426ede6daa43771eab905f21ff7363c0e74
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-11-10 11:45:00 -0500

    Demo ready seed files complete for backend

--- seed/seed.js
+++ seed/seed.js
@@ -7,20 +7,49 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 const main = async () => {
     const info =
         [
+
             {
-                "firstName": "Lorem",
-                "lastName": "Ipsum",
-                "streetAddress": "1 Murray Street",
-                "apartmentSuite": "#101",
+                "firstName": "Beyonce",
+                "lastName": "Knowles-Carter",
+                "streetAddress": "195 Hudson Street",
+                "apartmentSuite": "#39",
                 "city": "New York",
-                "county": "New York County",
-                "zipCode": "10007",
-                "day": "01",
-                "month": "01",
-                "year": "1970",
-                "email": "YahooHotmail@aol.com",
+                "county": "Manhattan",
+                "zipCode": "10013",
+                "day": "04",
+                "month": "09",
+                "year": "1981",
+                "email": "queenb@gmail.com",
                 "cell": "(123) 456-789"
             },
+            {
+                "firstName": "Taylor",
+                "lastName": "Swift",
+                "streetAddress": "155 Franklin Street",
+                "apartmentSuite": "#123",
+                "city": "New York",
+                "county": "Manhattan",
+                "zipCode": "10007",
+                "day": "13",
+                "month": "12",
+                "year": "1989",
+                "email": "youbelongwithme@aol.com",
+                "cell": "(123) 456-789"
+            },
+            {
+                "firstName": "Robyn",
+                "lastName": "Fenty",
+                "streetAddress": "125 Lafayette Street",
+                "apartmentSuite": "#4A",
+                "city": "New York",
+                "county": "Manhattan",
+                "zipCode": "10013",
+                "day": "02",
+                "month": "20",
+                "year": "1988",
+                "email": "riri@gmail.com",
+                "cell": "(123) 456-789"
+            }
         ]
     await Schema.insertMany(info)
     console.log("Seeded dummy info!")
